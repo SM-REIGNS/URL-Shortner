202da91e1742e78520b27033deb4900bdab0df47
Shubham Maity <38125965+SM-REIGNS@users.noreply.github.com>
2021-04-24 17:48:46 +0530

    Update BaseConversion.java

--- UrlShortener-API/src/main/java/com/shubham/urlshortenerapi/service/BaseConversion.java
+++ UrlShortener-API/src/main/java/com/shubham/urlshortenerapi/service/BaseConversion.java
@@ -27,10 +27,7 @@ public class BaseConversion {
     public long decode(String input) {
         var characters = input.toCharArray();
         var length = characters.length;
-
         var decoded = 0;
-
-        //counter is used to avoid reversing input string
         var counter = 1;
         for (int i = 0; i < length; i++) {
             decoded += allowedString.indexOf(characters[i]) * Math.pow(base, length - counter);
